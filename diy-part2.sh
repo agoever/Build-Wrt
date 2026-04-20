49644d8b1c36523841baf5ba71cedb383e4ed29f
agoever <128943290+agoever@users.noreply.github.com>
2026-04-20 19:07:46 +0800

    Change hostname from ImmortalWrt to HiWiFi

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,7 +29,7 @@ awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\|
 
 # 修改主机名 OP
 # sed -i 's/ImmortalWrt/ImmortalWrt $(TZ=UTC-8 date "+%y%m%d")/g'  package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/HiWrt/g'  package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/HiWiFi/g'  package/base-files/files/bin/config_generate
 
 
 # Delete default password
